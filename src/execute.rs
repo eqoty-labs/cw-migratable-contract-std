196e2daa2d603795363d7f3133f41baabe58e85a
Luca Spinazzola <mspinluca@gmail.com>
2023-03-09 20:36:14 -0500

    use secret-toolkit::storage

--- src/execute.rs
+++ src/execute.rs
@@ -1,9 +1,9 @@
 use cosmwasm_std::{
-    CanonicalAddr, ContractInfo, DepsMut, MessageInfo, Response, StdError, StdResult, Storage,
+    CanonicalAddr, ContractInfo, DepsMut, MessageInfo, Response, StdError, StdResult,
 };
 use schemars::_serde_json::to_string;
 use secret_toolkit::serialization::{Bincode2, Serde};
-use serde::de::DeserializeOwned;
+use secret_toolkit::storage::item::Item;
 
 use crate::msg_types::ReplyError::OperationUnavailable;
 use crate::state::{ContractMode, CONTRACT_MODE_KEY, NOTIFY_ON_MIGRATION_COMPLETE_KEY};
@@ -43,8 +43,10 @@ pub fn register_to_notify_on_migration_complete(
     code_hash: String,
     contract_mode: Option<ContractMode>,
 ) -> StdResult<Response> {
-    let contract_mode: ContractMode = if contract_mode.is_none() {
-        may_load(deps.storage, CONTRACT_MODE_KEY)?.unwrap()
+    let contract_mode = if contract_mode.is_none() {
+        Item::<ContractMode>::new(CONTRACT_MODE_KEY)
+            .may_load(deps.storage)?
+            .unwrap()
     } else {
         contract_mode.unwrap()
     };
@@ -59,8 +61,9 @@ pub fn register_to_notify_on_migration_complete(
             "This is an admin command and can only be run from the admin address",
         ));
     }
-    let mut contracts: Vec<ContractInfo> =
-        may_load(deps.storage, NOTIFY_ON_MIGRATION_COMPLETE_KEY)?.unwrap_or_default();
+    let mut contracts = Item::<Vec<ContractInfo>>::new(NOTIFY_ON_MIGRATION_COMPLETE_KEY)
+        .may_load(deps.storage)?
+        .unwrap_or_default();
     let mut update = false;
     let new_contract = ContractInfo {
         address: deps.api.addr_validate(address.as_str())?,
@@ -79,18 +82,4 @@ pub fn register_to_notify_on_migration_complete(
         );
     }
     Ok(Response::new())
-}
-
-/// Returns StdResult<Option<T>> from retrieving the item with the specified key.
-/// Returns Ok(None) if there is no item with that key
-///
-/// # Arguments
-///
-/// * `storage` - a reference to the storage this item is in
-/// * `key` - a byte slice representing the key that accesses the stored item
-fn may_load<T: DeserializeOwned>(storage: &dyn Storage, key: &[u8]) -> StdResult<Option<T>> {
-    match storage.get(key) {
-        Some(value) => Bincode2::deserialize(&value).map(Some),
-        None => Ok(None),
-    }
 }
