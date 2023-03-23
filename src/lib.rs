636f7f8089bf096db9fc16448e945aeebccfd535
Luca Spinazzola <mspinluca@gmail.com>
2023-03-22 23:50:33 -0400

    add query_migrated_info

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 pub mod execute;
 pub mod msg;
 pub mod msg_types;
+pub mod query;
 pub mod state;
 
 mod test_execute;
+mod test_query;
---
+++ src/query.rs
@@ -0,0 +1,34 @@
+use cosmwasm_std::{to_binary, Binary, Deps, StdResult};
+
+use crate::msg::MigratableQueryAnswer;
+use crate::state::{MIGRATED_FROM, MIGRATED_TO};
+
+/// Returns StdResult<Binary> displaying the Migrated to/from contract info
+///
+/// # Arguments
+///
+/// * `deps` - a reference to Extern containing all the contract's external dependencies
+/// * `migrated_from` - if migrated_from is true query returns info about the contract it was migrated
+/// from otherwise if returns info about the info the contract was migrated to
+pub(crate) fn query_migrated_info(deps: Deps, migrated_from: bool) -> StdResult<Binary> {
+    return match migrated_from {
+        true => {
+            let migrated_from = MIGRATED_FROM.may_load(deps.storage)?;
+            match migrated_from {
+                None => to_binary(&MigratableQueryAnswer::MigrationInfo(None)),
+                Some(some_migrated_from) => to_binary(&MigratableQueryAnswer::MigrationInfo(Some(
+                    some_migrated_from.contract,
+                ))),
+            }
+        }
+        false => {
+            let migrated_to = MIGRATED_TO.may_load(deps.storage)?;
+            match migrated_to {
+                None => to_binary(&MigratableQueryAnswer::MigrationInfo(None)),
+                Some(some_migrated_to) => to_binary(&MigratableQueryAnswer::MigrationInfo(Some(
+                    some_migrated_to.contract,
+                ))),
+            }
+        }
+    };
+}
