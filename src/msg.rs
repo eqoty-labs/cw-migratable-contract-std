65069512d18a1beae98827442729ecc961354c55
Luca Spinazzola <mspinluca@gmail.com>
2023-09-24 17:52:42 -0400

    remove Migrate, InstantiateByMigrationMsg, & MigrateFrom

--- src/msg.rs
+++ src/msg.rs
@@ -1,19 +1,10 @@
 use cosmwasm_std::{Binary, ContractInfo};
 use schemars::JsonSchema;
-use secret_toolkit::permit::Permit;
 use serde::{Deserialize, Serialize};
-
-use crate::msg_types::MigrateTo;
 
 #[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum MigratableExecuteMsg {
-    /// Set migration secret (using entropy for randomness), and the address of the new contract
-    Migrate {
-        /// permit used to verify address executing migration is admin
-        admin_permit: Permit,
-        migrate_to: MigrateTo,
-    },
     /// Sets a contract that should be notified when this contract completes the migration process
     SubscribeToMigrationCompleteEvent { address: String, code_hash: String },
     /// Triggers a MigrationCompleteNotification to be sent to the address specified. If this contract
--- src/msg_types.rs
+++ src/msg_types.rs
@@ -1,26 +1,11 @@
 use cosmwasm_std::Addr;
 use schemars::JsonSchema;
-use secret_toolkit::permit::Permit;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ReplyError {
     OperationUnavailable { message: String },
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct InstantiateByMigrationMsg {
-    pub migrate_from: MigrateFrom,
-    pub entropy: String,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq, JsonSchema)]
-pub struct MigrateFrom {
-    pub address: Addr,
-    pub code_hash: String,
-    /// permit for the  used to verify address executing migration is admin
-    pub admin_permit: Permit,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq, JsonSchema)]
