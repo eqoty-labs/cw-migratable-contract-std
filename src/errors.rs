e829d2f936f3c1412383722982557da99c17f5f1
Luca Spinazzola <mspinluca@gmail.com>
2023-10-26 02:47:12 -0400

    MigrationCompleteNotificationFailed error handling

---
+++ src/errors.rs
@@ -0,0 +1,29 @@
+use cosmwasm_std::{StdError, SubMsgResult};
+use thiserror::Error;
+
+#[derive(Error, Debug, PartialEq)]
+pub enum MigratableStdError {
+    #[error("{0}")]
+    // let thiserror implement From<StdError> for you
+    Std(#[from] StdError),
+    #[error("Migration complete event subscriber {0} has migrated. Request it to notify this contract of the new address. Error: {1}")]
+    MigrationCompleteNotificationFailed(String, String),
+}
+
+fn humanize_migration_complete_notification_error(
+    result: SubMsgResult,
+) -> Result<(), MigratableStdError> {
+    match result {
+        SubMsgResult::Ok(_) => Ok(()),
+        SubMsgResult::Err(error_msg) => {
+            if error_msg.contains("failed to validate") {
+                Err(MigratableStdError::MigrationCompleteNotificationFailed(
+                    "someaddress".to_string(),
+                    error_msg,
+                ))
+            } else {
+                Err(MigratableStdError::Std(StdError::generic_err(error_msg)))
+            }
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,5 @@ pub mod execute;
 pub mod msg;
 pub mod state;
 
+mod errors;
 mod test_execute;
